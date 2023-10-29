2be92d0e17726dc07712120366b10e557bf883ba
Deepak <85413425+anopenprogrammer@users.noreply.github.com>
2023-10-29 12:20:46 +0530

    adding new files

--- assets/index.js
+++ assets/index.js
@@ -15,9 +15,31 @@ window.addEventListener("load", () => {
     document.body.removeChild(loader);
   });
 });
+
 $(document).ready(function () {
   var loader = $(".loader");
-  loader.click(function(){
+  loader.click(function () {
     loader.hide();
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var loginForm = document.getElementById("loginForm");
+
+  loginForm.addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent the default form submission behavior
+
+      var idInput = document.getElementById("id");
+      var passwordInput = document.getElementById("password");
+
+      var enteredId = idInput.value;
+      var enteredPassword = passwordInput.value;
+
+      if (enteredId === "Deepak" && enteredPassword === "Deepak") {
+          window.location.href = "healthcareManufacturer.html";
+      } else {
+          alert("Invalid ID or Password. Please try again.");
+      }
+  });
+});
+
