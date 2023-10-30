f9e1e4048076d4775d27319327edb516c6026e9f
Deepak <85413425+anopenprogrammer@users.noreply.github.com>
2023-10-30 12:53:39 +0530

    adding new files

--- assets/index.js
+++ assets/index.js
@@ -27,19 +27,48 @@ document.addEventListener("DOMContentLoaded", function () {
   var loginForm = document.getElementById("loginForm");
 
   loginForm.addEventListener("submit", function (event) {
-      event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault(); // Prevent the default form submission behavior
 
-      var idInput = document.getElementById("id");
-      var passwordInput = document.getElementById("password");
+    var idInput = document.getElementById("id");
+    var passwordInput = document.getElementById("password");
 
-      var enteredId = idInput.value;
-      var enteredPassword = passwordInput.value;
+    var enteredId = idInput.value;
+    var enteredPassword = passwordInput.value;
 
-      if (enteredId === "Deepak" && enteredPassword === "Deepak") {
-          window.location.href = "healthcareManufacturer.html";
-      } else {
-          alert("Invalid ID or Password. Please try again.");
-      }
+    if (enteredId === "Deepak" && enteredPassword === "Deepak@123") {
+      window.location.href = "healthcareManufacturer.html";
+    } else {
+      alert("Invalid ID or Password. Please try again.");
+    }
   });
 });
 
+// function showPopup() {
+//   document.getElementById("popup").style.display = "block";
+//   document
+//     .getElementById("popup")
+//     .setAttribute("class", "model-container scale-up-center");
+// }
+function showPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+  popup.classList.remove("scale-down-center");
+  popup.classList.add("scale-up-center");
+}
+function hidePopup() {
+  var popup = document.getElementById("popup");
+  popup.classList.remove("scale-up-center");
+  popup.classList.add("scale-down-center");
+
+  popup.style.display = "none";
+}
+
+// function hidePopup() {
+//   document
+//     .getElementById("popup")
+//     .setAttribute("class", "model-container scale-down-center");
+//   document.getElementById("popup").style.display = "none";
+// }
+// function scalingdown(){
+
+// }
